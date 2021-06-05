565e2184213de78720fa45b952eafd7a0cac3fb0
OjasviChauhan <ojasvichauhan2304@gmail.com>
2021-06-06 00:52:33 +0530

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Weather from "./components/Weather";
+import { weatherActions } from "./store/weatherSlice";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const weatherData = useSelector((state) => state.weather.weather_data);
+
+  useEffect(() => {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Lucknow&appid=c0ce945fbd27b35f620422f7b0dc33d6&units=metric"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        dispatch(weatherActions.weatherDataReducer(data));
+      });
+  }, []);
+
+  return <Weather data={weatherData} />;
 }
 
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Weather.css";
+
+function Weather(props) {
+  return (
+    <div className="weather">
+      <div className="weather__data">
+        <h1 className="weather__temp">
+          {props.data?.main?.temp} Degree celcius
+        </h1>
+        <h5 className="weather__city">{props.data?.name}, India</h5>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
